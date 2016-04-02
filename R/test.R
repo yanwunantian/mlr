cca2ea8d0c4501bb5e59a473dc9a963e0f6815ed
Arne Bethmann <arne.bethmann@gmx.de>
2016-04-02 23:00:57 +0200

    initial comparison of ICEbox example with mlr Partial Predictions

---
+++ R/test.R
@@ -0,0 +1,35 @@
+library(mlr)
+library(ICEbox)
+library(randomForest)
+library(MASS) #has Boston Housing data, Pima
+
+
+# ICEbox example
+set.seed(20160402)
+
+########  regression example
+data(Boston) #Boston Housing data
+X = Boston
+y = X$medv
+X$medv = NULL
+
+## build a RF:
+bhd_rf_mod = randomForest(X, y)
+
+
+## usually crashes!!!
+## Create an 'ice' object for the predictor "age":
+bhd.ice = ice(object = bhd_rf_mod, X = X, y = y, predictor = "age", frac_to_build = .1)
+
+plot(bhd.ice)
+
+
+# mlr example
+set.seed(20160402)
+
+task = makeRegrTask(id = "bhd", data = Boston, target = "medv")
+lrn = makeLearner("regr.randomForest")
+mod = train(lrn, task)
+pd.ind.regr = generatePartialPredictionData(mod, task, c("age"), individual = TRUE)
+plotPartialPrediction(pd.ind.regr)
+

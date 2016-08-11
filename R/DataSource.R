3bc5a75da0b31c6a5934e647bc03428734ec43b3
Michel Lang <michellang@gmail.com>
2016-08-11 11:53:49 +0200

    tests, man pages, ...

--- R/DataSource.R
+++ R/DataSource.R
@@ -1,74 +1,95 @@
+#' @title Create a Data Source backend for mlr Tasks.
+#'
+#' @param src [\code{\link[base]{data.frame}} | \code{\link[dplyr]{tbl}}]\cr
+#'   Source of the data. Currently supported
+#'   \itemize{
+#'     \item[\code{\link[base]{data.frame}}]{Regular data.frames.}
+#'     \item[\code{\link[dplyr]{tbl}}]{Tables as used in \pkg{dplyr}. Can use a SQL backend.}
+#'   }
+#' @param rows [\code{integer} | \code{NULL}]\cr
+#'   Subset of rows to use in the task. Additional rows are ignored.
+#' @param cols [\code{character} | \code{NULL}]\cr
+#'   Subset of columns to use in the task. Additional columns in \code{src} are ignored.
+#' @param key [\code{character}]\cr
+#'   Required for database backends: Name of the primary key column.
+#' @param ... [\code{any}]\cr
+#'   Additional arguments, depending on the type of \code{src}.
+#' @return [\code{DataSource}].
+#' @alias DataSource
 #' @export
-makeDataSource = function(x, cols = NULL, ...) {
+makeDataSource = function(src, rows = NULL, cols = NULL, ...) {
   UseMethod("makeDataSource")
 }
 
 #' @export
-makeDataSource.data.frame = function(x, cols = NULL, ...) {
+makeDataSource.data.frame = function(src, rows = NULL, cols = NULL, ...) {
   assertCharacter(cols, any.missing = FALSE, null.ok = TRUE)
   ee = new.env(parent = emptyenv())
-  ee$data = x
-  ee$cols = cols %??% names(x)
-  class(ee) = c("DataLocal", "Data")
+  ee$data = indexHelper(src, rows, cols)
+  ee$cols = cols %??% names(src)
+  class(ee) = c("DataSourceDataFrame", "DataSource")
   ee
 }
 
 #' @export
-makeDataSource.tbl = function(x, cols = NULL, key = NULL, ...) {
+makeDataSource.tbl = function(src, rows = NULL, cols = NULL, key = NA_character_, ...) {
+  requireNamespace("dplyr")
+  requireNamespace("lazyeval")
+
   assertCharacter(cols, any.missing = FALSE, null.ok = TRUE)
   assertString(key)
   ee = new.env(parent = emptyenv())
-  ee$data = x
-  ee$cols = cols %??% setdiff(colnames(x), key)
-  ee$ids = collect(select_(x, key))[[1L]]
+  ids = dplyr::collect(dplyr::select_(src, key))[[1L]]
+  ee$data = src
+  ee$cols = cols %??% setdiff(colnames(src), key)
+  ee$ids = if (is.null(rows)) ids else intersect(ids, rows)
   ee$key = key
-  class(ee) =  c("DataRemote", "Data")
+  class(ee) =  c("DataSourceTbl", "DataSource")
   ee
 }
 
-getData = function(data, subset, features) {
+#' @title Retrive data from a DataSource.
+#'
+#' @param data [\code{\link{DataSource}}]\cr
+#'    \code{\link{DataSource}}.
+#' @param subset [\code{integer}]\cr
+#'   Subset of rows to retrieve.
+#' @param features [\code{character}]\cr
+#'   Names of features to retrieve.
+#' @return [\code{data.frame}].
+#' @export
+getData = function(src, subset, features) {
   UseMethod("getData")
 }
 
-getData.DataLocal = function(data, subset = NULL, features = NULL) {
-  switch(2L * is.null(subset) + is.null(features) + 1L,
-      data$data[subset, features, drop = FALSE],
-      data$data[subset, , drop = FALSE],
-      data$data[ , features, drop = FALSE],
-      data$data)
+#' @export
+getData.DataSourceDataFrame = function(data, subset = NULL, features = NULL) {
+  indexHelper(data$data, subset, features)
 }
 
-getData.DataRemote = function(data, subset = NULL, features = NULL) {
-  if (is.null(subset)) {
-    res = data$data
+#' @export
+getData.DataSourceTbl = function(data, subset = NULL, features = NULL) {
+  tmp = is.null(subset) && is.null(data$ids)
+  if (is.null(subset) && is.null(data$ids)) {
+      res = data$data
   } else {
-    res = filter_(data$data, interp("key %in% ids", key = as.name(data$key), ids = subset))
+    ids = if (!is.null(subset) && !is.null(data$ids)) intersect(subset, data$ids) else subset %??% data$ids
+    res = dplyr::filter_(data$data, lazyeval::interp("key %in% ids", key = as.name(data$key), ids = ids))
   }
 
   if (is.null(features)) {
-    res = select_(res, ~one_of(data$cols))
+    res = dplyr::select_(res, ~dplyr::one_of(data$cols))
   } else {
     assertSubset(features, data$cols)
-    res = select_(res, ~one_of(features))
+    res = dplyr::select_(res, ~dplyr::one_of(features))
   }
-  collect(res)
+  dplyr::collect(res)
 }
 
-
-if (FALSE) {
-  library(dplyr)
-  library(lazyeval)
-  library(checkmate)
-
-  db = src_sqlite(tempfile(fileext = ".sql"), create = T)
-  df = iris
-  df$.row = seq_len(nrow(df))
-  src = copy_to(db, df, name = "data", temporary = FALSE, indexes = list(".row"))
-  x = tbl(db, "data")
-
-  data = makeDataSource(x, key = ".row")
-  getData(data, 20:60, features = "Species")
-
-  data = makeDataSource(iris)
-  getData(data, 20:60)
+indexHelper = function(data, i, j) {
+  switch(2L * is.null(i) + is.null(j) + 1L,
+      data[i, j, drop = FALSE],
+      data[i, , drop = FALSE],
+      data[ , j, drop = FALSE],
+      data)
 }

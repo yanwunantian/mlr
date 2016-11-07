59ccdb378a23d1f4d4c8b561c495fe5ec237875b
Laura <laura.beggel@de.bosch.com>
2016-11-07 09:20:13 +0100

    demo_file.Rmd added

--- test_curve_with_preproc.R
+++ test_curve_with_preproc.R
@@ -7,6 +7,7 @@ gp = load2("gunpoint.RData")
 
 task = makeTimeSeriesClassifTask(data = gp, target = "X1", positive = "1")
 task.wd = makeTSFeaturesClassifTask(task = task, method = "wavelets", pars = list(filter = "haar"))
+task.ft = makeTSFeaturesClassifTask(task = task, method = "fourier", pars = list(fft.coeff = "amplitude"))
 
 lrn = makeLearner("classif.rpart")
 
@@ -15,6 +16,12 @@ lrn = makeLearner("classif.rpart")
 # classification on wavelet features
 model = train(lrn, task.wd, subset = 1:50)
 pred = predict(model, task.wd, subset = 51:200)
+p = performance(pred, measures = list(mmce, tpr))
+print(p)
+
+# classification on fourier features
+model = train(lrn, task.ft, subset = 1:50)
+pred = predict(model, task.ft, subset = 51:200)
 p = performance(pred, measures = list(mmce, tpr))
 print(p)
 

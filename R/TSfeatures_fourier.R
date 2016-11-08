d12dd71bade655387e79b8576eb4a8cc710d5af7
smilesun <smilesun.east@gmail.com>
2016-11-08 15:40:33 +0100

    Laura and Xudong generate the first unit test

--- R/TSfeatures_fourier.R
+++ R/TSfeatures_fourier.R
@@ -21,21 +21,25 @@ getTSFourierFeatures = function(curves, fft.coeff = NULL) {
     stop("Transformation for complex frequency domain must be one of 'amplitude' or 'phase'. Please check method.")
 
 
-  fftdata = fft(as.matrix(curves))
-  fftdata = as.data.frame(fftdata)
+  fftdata = t(apply(as.matrix(curves),1, fft))
+  fftdata=as.data.frame(fftdata)
 
   switch(fft.coeff,
          amplitude = {ffttrafo = getFourierAmplitude(fourier.comp = fftdata)},
          phase = {ffttrafo = getFourierPhase(fourier.comp = fftdata)}
          )
   print(fft.coeff)
-  ffttrafo = as.data.frame(ffttrafo)
+  
+  if(!inherits(ffttrafo, "matrix"))
+    ffttrafo = as.data.frame(matrix(ffttrafo, nrow = 1)) else
+      ffttrafo = as.data.frame(ffttrafo)
+  
   return(ffttrafo)
 }
 
 #' @export
 getFourierAmplitude = function(fourier.comp){
-  amp = apply(fourier.comp, 2, function(x) Re(x)^2 + Im(x)^2 )
+  amp = apply(fourier.comp, 2, function(x) Re(x)^2 + Im(x)^2 ) # compute for each row
   amp = sqrt(amp)
   return(amp)
 }

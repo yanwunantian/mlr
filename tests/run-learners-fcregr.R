1e60f3ae0f3fa72314b89a0e4b4be340ae5f3366
Steve <stevo15025@gmail.com>
2016-12-20 16:15:20 -0500

    Trying out gcDLLs() in learner tests

--- tests/run-learners-fcregr.R
+++ tests/run-learners-fcregr.R
@@ -1,5 +1,6 @@
 library(testthat)
 if (identical(Sys.getenv("TRAVIS"), "true") || identical(Sys.getenv("R_EXPENSIVE_TEST_OK"), "true")) {
+  R.utils::gcDLLs()
   test_check("mlr", "_learners_all_fcregr")
 }
 
